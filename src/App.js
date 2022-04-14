f770a65bbdd1a466f299e20dbd87d1254ff50575
Max ChZ <chzanibal@hotmail.com>
2022-04-13 21:27:25 -0300

    agregar explicacion de sass

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import './App.css';
+import "./styles/styles.scss";
 
 function App() {
   return (
     <div className="App">
-      <div className="flex flex-col md:flex-row">
-        <div className="mx-10 bg-green-300">Elemento 1</div>
-        <div className="mx-10 bg-violet-500">Elemento 2</div>
-        <div className="mx-10 bg-orange-500">Elemento 3</div>
-        <div className="mx-10 bg-blue-500">Elemento 4</div>
-        <button className="border-2 border-blue-900 p-3 bg-violet-900 text-white hover:bg-yellow-600 hover:scale-2 rounded-lg shadow-lg">Click here!!</button>
-        <button className="border-2 border-blue-900 p-3 bg-violet-900 text-white hover:bg-yellow-600 hover:scale-2 rounded-lg shadow-lg">Boton numero 2!!</button>
+      <div className="fondoNegro1">Este es un fondo negro 1</div>
+      <div className="fondoNegro2">Este es un fondo negro 2</div>
 
-      </div>
+      
        
     </div>
   );
