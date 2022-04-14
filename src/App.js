20be0abb5e1b0c3c08c79b5afa04b318e35ffced
Max ChZ <chzanibal@hotmail.com>
2022-04-13 22:58:24 -0300

    agrega explicaicon de formularios basico 1

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="flex flex-col md:flex-row">
-        <div className="mx-10 bg-green-300">Elemento 1</div>
-        <div className="mx-10 bg-violet-500">Elemento 2</div>
-        <div className="mx-10 bg-orange-500">Elemento 3</div>
-        <div className="mx-10 bg-blue-500">Elemento 4</div>
-        <button className="border-2 border-blue-900 p-3 bg-violet-900 text-white hover:bg-yellow-600 hover:scale-2 rounded-lg shadow-lg">Click here!!</button>
-        <button className="border-2 border-blue-900 p-3 bg-violet-900 text-white hover:bg-yellow-600 hover:scale-2 rounded-lg shadow-lg">Boton numero 2!!</button>
+      <form>
+        <input type="email"/>
+        <input type="file"/>
+        <input/>
+        <input/>
+        <input type="text" required/>
+        <button type="submit">Submit</button>
+        <select required defaultValue={0}> 
+          <option disabled value={0}> Selecciones una opcion</option>
+          <option> opcion 1</option>
+          <option> opcion 2</option>
 
-      </div>
+        </select>
+
+
+      </form>
+     
+      
        
     </div>
   );
